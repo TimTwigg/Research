07cb15c08aa437a587607b7a33866eeb38625d26
vitonash <80741323+vitonash@users.noreply.github.com>
2022-04-07 11:53:13 -0700

    deleted unneeded files

--- opencv/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-y = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# c = 0
-# for i in cnts:
-#     area = cv2.contourArea(i)
-#     if area > 1000/2:
-#         cv2.drawContours(warpedImgColor, cnts, c, (0, 0, 0), 2)
-#     c += 1
-# cv2.imshow("
