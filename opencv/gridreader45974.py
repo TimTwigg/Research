19d0ec488b0415ece6f66630c721076d5ddfb609
vitonash <80741323+vitonash@users.noreply.github.com>
2022-05-12 01:12:46 -0700

    made this bitch a class

--- opencv/gridreader45974.py
+++ opencv/gridreader45974.py
@@ -2,75 +2,117 @@ import numpy as np
 import cv2
 import os
 
-def reorder(points):
-    points = points.reshape((4, 2))
-    npoints = np.zeros((4, 1, 2), dtype=np.int32)
-    add = points.sum(1)
-    npoints[0] = points[np.argmin(add)]
-    npoints[3] = points[np.argmax(add)]
-    diff = np.diff(points, axis=1)
-    npoints[1] = points[np.argmin(diff)]
-    npoints[2] = points[np.argmax(diff)]
-    return npoints
+class GridReader:
+    def __init__(self, image: str, squares_on_side: int):
+        '''
+        image -- path to theimage
+        squares_on_side -- number of squares on a single side of the grid
+        '''
+        self.__image = cv2.imread(image)
+        self.__squares_on_side = squares_on_side # num squares on a single side of the grid
+        self.__widthImg = self.__heightImg = self.__squares_on_side * 50 + 50 # size of the image in pixels
+        self.__grid = np.zeros((self.__squares_on_side, self.__squares_on_side), dtype=int)
+        self.__warped = self.warp(self.threshImage())
 
-# imports image
-current_dir = os.getcwd()
-print(current_dir)
-image = cv2.imread("grid1red.png")
-side_count = 11 # num squares on a single side of the grid
-widthImg = heightImg = side_count * 50 + 50
-# converts to monochrome
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#blurs image slightly
-# blur = cv2.GaussianBlur(gray, (5,5), 0)
-# adaptive thresholding                             this digit controls how much gets subtracted
-thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 13, 10)
 
-# finds all contours
-cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-# finds the largest square, the border of the grid
-max_area = 0
-c = 0
-for i in cnts:
-    area = cv2.contourArea(i)
-    if area > 1000:
-        permiter = cv2.arcLength(i, True)
-        approx = cv2.approxPolyDP(i, .02 * permiter, True)
-        if area > max_area and len(approx) == 4: 
-            max_area = area
-            biggest = i
-    c += 1
-
-# warps perspective to ensure the gridlines are straight as possible
-if biggest.size != 0:
-    biggest = reorder(biggest)
-    pts1 = np.float32(biggest)
-    pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
-    matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    warpedImgColor = cv2.warpPerspective(image, matrix, (widthImg, heightImg))
-    warpedImgBW = cv2.cvtColor(warpedImgColor, cv2.COLOR_BGR2GRAY)
-
-def isRed(color):
-    if color[0] not in range(0,7):
-        return False
-    if color[1] not in range(0, 100):
-        return False
-    if color[2] not in range(100, 256):
-        return False
-    return True
-
-grid = np.zeros((side_count, side_count), dtype=int)
-
-for x in range (1, side_count + 1):
-    for y in range (1, side_count + 1):
-        cv2.circle(warpedImgColor, (50 * x, 50 * y), 2, (0, 255, 0))
-        if isRed(warpedImgColor[x * 50, y * 50]):
-            grid[x - 1, y - 1] = 1
-
+    def reorder(self, points: np.ndarray) -> np.ndarray:
+        '''
+        Takes an array of points and orders them properly from left -> right, top -> bottom. 
         
+        points -- array of points to reorder
 
-cv2.imshow("image", warpedImgColor)
-print(grid)
+        returns: sorted array of points
+        '''
+        points = points.reshape((4, 2))
+        npoints = np.zeros((4, 1, 2), dtype=np.int32)
+        add = points.sum(1)
+        npoints[0] = points[np.argmin(add)]
+        npoints[3] = points[np.argmax(add)]
+        diff = np.diff(points, axis=1)
+        npoints[1] = points[np.argmin(diff)]
+        npoints[2] = points[np.argmax(diff)]
+        return npoints
 
-cv2.waitKey(0)
+    def threshImage(self) -> "image":
+        '''
+        Takes a source image and converts it to monochrome, blurs it, and applies adaptive thresholding. 
+        It then returns the result of this processing.
+
+        returns: image 
+        '''
+        # converts to monochrome
+        gray = cv2.cvtColor(self.__image, cv2.COLOR_BGR2GRAY)
+        # blurs image slightly
+        blur = cv2.GaussianBlur(gray, (5,5), 0)
+        # adaptive thresholding                             this digit controls how much gets subtracted
+        thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 13, 10)
+
+        return thresh
+
+    def warp(self, thresh: "image") -> "image":
+        '''
+        Takes a threshed image as input and warps it to class specified size and returns the warped image.
+
+        thresh -- threshed image of grid
+
+        returns: image (self.widthImg x self.heightImg)
+        '''
+        # finds all contours
+        cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # finds the largest square, the border of the grid
+        max_area = 0
+        c = 0
+        for i in cnts:
+            area = cv2.contourArea(i)
+            if area > 1000:
+                permiter = cv2.arcLength(i, True)
+                approx = cv2.approxPolyDP(i, .02 * permiter, True)
+                if area > max_area and len(approx) == 4: 
+                    max_area = area
+                    biggest = i
+            c += 1
+
+        # warps perspective to ensure the gridlines are straight as possible
+        if biggest.size != 0:
+            biggest = self.reorder(biggest)
+            pts1 = np.float32(biggest)
+            pts2 = np.float32([[0, 0], [self.__widthImg, 0], [0, self.__heightImg], [self.__widthImg, self.__heightImg]])
+            matrix = cv2.getPerspectiveTransform(pts1, pts2)
+            return cv2.warpPerspective(self.__image, matrix, (self.__widthImg, self.__heightImg))
+
+    def isRed(self, color: list[int]) -> bool:
+        '''
+        Checks if provided BGR color is red or not, then returns true or false.
+
+        color -- a list of 3 ints representing BGR color
+
+        returns: bool, true if red, false if not
+        '''
+        if color[0] not in range(0,7):
+            return False
+        if color[1] not in range(0, 100):
+            return False
+        if color[2] not in range(100, 256):
+            return False
+        return True
+
+    def readGrid(self) -> np.ndarray:
+        '''
+        Reads the grid image. If there are squares containing red, their number in the grid
+        is changed to a 1. Returns the grid.
+
+        returns: 2d array, 0 denoting white, 1 denoting red
+        '''
+        for x in range (1, self.__squares_on_side + 1):
+            for y in range (1, self.__squares_on_side + 1):
+                cv2.circle(self.__warped, (50 * x, 50 * y), 2, (0, 255, 0))
+                if self.isRed(self.__warped[x * 50, y * 50]):
+                    self.__grid[x - 1, y - 1] = 1
+        return(self.__grid)
+
+if __name__ == '__main__':
+    r = GridReader('grid1red.png', 11)
+    
+    print(r.readGrid())
+
