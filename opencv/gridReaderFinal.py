a3e64ef00c266fad6a6a015ec85eee51f4ef9ae1
vitonash <80741323+vitonash@users.noreply.github.com>
2022-05-25 22:38:23 -0700

    created venv

--- opencv/gridReaderFinal.py
+++ opencv/gridReaderFinal.py
@@ -125,14 +125,16 @@ class GridReader:
         '''
         for x in range (1, self.__squares_on_side + 1):
             for y in range (1, self.__squares_on_side + 1):
-                cv2.circle(self.__warped, (50 * x, 50 * y), 2, (0, 255, 0))
+                print(self.__widthImg/ self.__squares_on_side)
+                cv2.circle(self.__warped, (((self.__widthImg / self.__squares_on_side) // 1) * x - 25, 52 * y - 25), 2, (255, 175, 125))
                 if self.isRed(self.__warped[x * 50, y * 50]):
                     self.__grid[x - 1, y - 1] = 1
+        cv2.imshow('help', self.__warped)
+        cv2.waitKey(0)
         return(self.__grid)
 
 if __name__ == '__main__':
     #r = GridReader('grid1red.png', 1y)
     r = GridReader('opencv_frame_0.png', 18)
-    
     print(r.readGrid())
 
