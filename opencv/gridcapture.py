d36d3c243796e6695980968f4e1f92cfd2617998
bgurkas <bgurkas@uci.edu>
2022-05-25 23:27:51 -0700

    Made file into a function named "screenshot" which returns path to image taken. Had to play with line 8 to get the camera I wanted. The window is now able to be closed when the user presses "X" button on window

--- opencv/gridcapture.py
+++ opencv/gridcapture.py
@@ -2,31 +2,41 @@
 
 import cv2
 
-cam = cv2.VideoCapture('/dev/video2')
+def screenshot():
+    '''Returns the path to the captured image.'''
+    return_path = ""
+    cam = cv2.VideoCapture(0)
 
-cv2.namedWindow("test")
+    cv2.namedWindow("test")
 
-img_counter = 0
-
-while True:
     ret, frame = cam.read()
     if not ret:
         print("failed to grab frame")
-        break
-    cv2.imshow("test", frame)
+    else:
+        while True:
+            ret, frame = cam.read()
+            cv2.imshow("test", frame)
 
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+            k = cv2.waitKey(1)
+            if k%256 == 27:
+                # ESC pressed
+                print("Escape hit, closing...")
+                break
+                
+            elif k%256 == 32:
+                # SPACE pressed
+                img_name = "opencv_frame_0.png"
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+                return_path = img_name
+            
+            if cv2.getWindowProperty("test", cv2.WND_PROP_VISIBLE) < 1:
+                print("User pressed X on the window")
+                break
 
-cam.release()
+    cam.release()
+    cv2.destroyAllWindows()
+    return return_path
 
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    screenshot()
