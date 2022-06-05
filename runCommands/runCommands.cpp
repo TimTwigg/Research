3509d79fb7f902efb22ad402f143d520e8645634
TimTwigg <63125914+TimTwigg@users.noreply.github.com>
2022-06-05 01:19:19 -0700

    Work on runCommands

--- runCommands/runCommands.cpp
+++ runCommands/runCommands.cpp
@@ -1,11 +1,47 @@
-// updated 3 June 2022
+// updated 5 June 2022
 // C++ module to run commands by simulating keyboard input
 
 #include <string>
 #include <windows.h>
 #include <iostream>
 #include <fstream>
-#include <conio.h>
+#include <map>
+#include <time.h>
+
+bool write(char c) {
+    // Sam's answer at
+    // https://stackoverflow.com/questions/2167156/sendinput-isnt-sending-the-correct-shifted-characters
+    
+    INPUT input[ 2 ];
+
+    input[0].type = INPUT_KEYBOARD;
+    input[0].ki.wVk = 0;
+    input[0].ki.wScan = c;
+    input[0].ki.dwFlags = KEYEVENTF_UNICODE;
+    input[0].ki.time = 0;
+    input[0].ki.dwExtraInfo = 0;
+
+    input[1].type = INPUT_KEYBOARD;
+    input[1].ki.wVk = 0;
+    input[1].ki.wScan = c;
+    input[1].ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
+    input[1].ki.time = 0;
+    input[1].ki.dwExtraInfo = 0;
+
+    SendInput(2, input, sizeof(INPUT));
+    return true;
+}
+
+void enter() {
+    INPUT ip;
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.time = 0;
+    ip.ki.dwFlags = KEYEVENTF_UNICODE;
+    ip.ki.wScan = VK_RETURN;
+    ip.ki.wVk = 0;
+    ip.ki.dwExtraInfo = 0;
+    SendInput(1, &ip, sizeof(INPUT));
+}
 
 // run by calling exe with single arg of the filename to run
 int main(int argc, char** argv) {
@@ -21,22 +57,23 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    char c = getch();
+    Sleep(2000);
 
     while (in) {
         std::string command;
         std::getline(in, command);
-        
-        // replace this with simulating keybaord rather than sending to cout
-        std::cout << command << std::endl;
-        INPUT ip;
-        ip.type = INPUT_KEYBOARD;
-        ip.ki.time = 0;
-        ip.ki.dwExtraInfo = 0;
+        if (command.size() < 1) break;
 
-        // need a map to get the right code, then press, then release
-        // gonna need to loop through each string
+        // this method of writing individual characters is reliable (with the sleep) but slow
+        write('\\');
+        for (char c : command) {
+            write(c);
+            Sleep(1); // without this is works nicely for a while then goes mad
+        }
+        enter();
     }
+
+    std::cout << "complete" << std::endl;
 
     return 0;
 }
