5e356346fa5b2571a33ecdd1d948281fe924d940
Tim Twigg <tim@twiggusa.com>
2022-11-03 12:46:43 -0700

    Incorporate image-taking into App2
    Also add some extra error-catching in main and better error description in MazeGeneration\generate.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# updated 26 October 2022
+# updated 3 November 2022
 
 from App2 import App
 
@@ -7,35 +7,38 @@ import usb.util
 
 def listen(dev, endpoint_in, endpoint_out):
     app = App()
+    app.grabCamera()
     while(True):
         try:
-            buf = dev.read(endpoint_in.bEndpointAddress,64,1000)
+            buf = dev.read(endpoint_in.bEndpointAddress, 64, 1000)
             buf = bytearray(buf)
-            print(buf)
             code = buf[1]
             if code == 0:
                 continue
             elif code == 1:
                 app.callMaze()
             elif code == 2:
-                print("here!")
                 app.callSound()
         except usb.core.USBTimeoutError as e:
             pass
+        except NameError:
+            # catches the error thrown by gridReaderFinal when it doesn't find
+            # the grid and the 'biggest' variable is not defined.
+            print("Could not interpret image. Re-focus camera")
 
 def start():
-    dev = usb.core.find(idVendor=0x16C0, idProduct=0x0486)
+    dev = usb.core.find(idVendor = 0x16C0, idProduct = 0x0486)
 
     if dev is None:
         raise ValueError("Device not found")
 
-    endpoint_in = dev[0][(0,0)][0]
-    endpoint_out = dev[0][(0,0)][1]
+    endpoint_in = dev[0][(0, 0)][0]
+    endpoint_out = dev[0][(0 ,0)][1]
 
     try:
         dev.reset()
     except Exception as e:
-        print('reset', e)
+        print("reset", e)
 
     listen(dev, endpoint_in, endpoint_out)
 
