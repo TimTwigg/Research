379279638cb5c9703c04babbc289598f4cfc5dbe
Tim Twigg <tim@twiggusa.com>
2023-05-03 16:27:07 -0700

    fix camera selection

--- gooey/src/components/camera.jsx
+++ gooey/src/components/camera.jsx
@@ -5,6 +5,12 @@ const Camera = () => {
     const [devices, SetDevices] = useState([]);
     const [deviceID, SetDeviceID] = useState("");
 
+    const videoConstraints = {
+        width: 1280,
+        height: 720,
+        deviceId: deviceID
+    }
+
     const handleDevices = useCallback(mediaDevices => {
         SetDevices(mediaDevices.filter(({ kind }) => kind === "videoinput"));
     }, [SetDevices]);
@@ -15,8 +21,8 @@ const Camera = () => {
 
     return (
         <div className = "container Camera">
-            <Webcam className = "six columns marginB"></Webcam>
-            <select className = "six columns" onChange = {item => SetDeviceID(item.value)}>
+            <Webcam className = "six columns marginB" videoConstraints = {videoConstraints}/>
+            <select className = "six columns" onChange = {item => SetDeviceID(item.target.value)}>
                 {
                     devices.map((dev, i) => <option key = {i} value = {dev.deviceId}>{dev.label}</option>)
                 }
