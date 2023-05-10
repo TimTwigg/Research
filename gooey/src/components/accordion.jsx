a02fccecf976ab964041f093c6cc6d61333f8b4c
vitonash <vitocnash@gmail.com>
2023-05-10 15:47:53 -0700

    worked on help page

---
+++ gooey/src/components/accordion.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const Accordion = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="accordion">
+      <div className="accordion-header" onClick={toggleAccordion}>
+        <h2>{title}</h2>
+        <span className={`icon ${isOpen ? 'open' : ''}`}></span>
+      </div>
+      {isOpen && (
+        <div className="accordion-body">
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Accordion;
