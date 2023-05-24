c49b75cab313183763f746836a1b7f60ae92aac6
Tim Twigg <tim@twiggusa.com>
2023-05-24 16:14:35 -0700

    fix bug:

--- gooey/src/components/camera.jsx
+++ gooey/src/components/camera.jsx
@@ -38,10 +38,7 @@ const Camera = () => {
     const TakeImage = async (img) => {
         SetEnableCamera(false);
         SetImage(img);
-        const arg_gridsize = gridSize;
-        const arg_deviceID = deviceID;
-        // fetch("http:localhost:5000/data/" + JSON.stringify(data)).then(res => res.json().then(data => {console.log(data)}))
-        const response = await fetch(`http://localhost:5000/data?gridsize=${arg_gridsize}&deviceID=${arg_deviceID}`);
+        const response = await fetch(`http://localhost:5000/data?gridsize=${gridSize}&deviceID=${deviceID}&falsePaths=${falsePaths}`);
         SetEnableCamera(true);
     }
 
