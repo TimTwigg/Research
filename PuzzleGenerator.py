dc25c06d73d4fb0d4c5cd5405196acc5cbeb29de
Tim Twigg <tim@twiggusa.com>
2023-10-09 13:06:17 -0700

    Document PuzzleGenerator

--- PuzzleGenerator.py
+++ PuzzleGenerator.py
@@ -1,4 +1,4 @@
-# updated 29 May 2023
+# updated 9 October 2023 2023
 
 import subprocess
 import cv2
@@ -9,16 +9,22 @@ from runCommands.refocus import setFocus
 import opencv.gridReaderFinal as gr
 
 class PuzzleGenerator:
-    def __init__(self, board_size:int = 15):
-        self._grid_size = board_size
-        self._x = 0
-        self._y = -50
-        self._z = 0
-        self._grid = None
-        self._commands = []
-        self.hasCamera = False
-        self.gotBlank = False
-        self._blank_file_ = None
+    """Generator for Minecraft Puzzle Generation"""
+    def __init__(self, board_size: int = 15):
+        """Create a PuzzleGenerator
+
+        Args:
+            board_size (int, optional): the length/width of the square grid. Defaults to 15.
+        """
+        self._grid_size: int = board_size
+        self._x: int = 0
+        self._y: int = -50
+        self._z: int = 0
+        self._grid: list[list[int]] = None
+        self._commands: list[str] = []
+        self.hasCamera: bool = False
+        self.gotBlank: bool = False
+        self._blank_file_: str = None
         
     def __del__(self):
         try:
@@ -27,22 +33,43 @@ class PuzzleGenerator:
             pass
     
     def setSize(self, board_size: int):
+        """Set/Change the grid board size
+
+        Args:
+            board_size (int): the new size
+        """
         self._grid_size = board_size
     
     def setBlank(self, filename: str):
+        """Set the black filename
+
+        Args:
+            filename (str): filename of blank file
+        """
         self._blank_file_ = filename
         
     def grabCamera(self, cameraID: int = 0):
+        """Grab a camera
+
+        Args:
+            cameraID (int, optional): the id of the camera on the local system. Defaults to 0.
+        """
         self._cam = cv2.VideoCapture(cameraID, cv2.CAP_DSHOW)
         self.hasCamera = True
         print("Grabbed Camera")
         
     def releaseCamera(self):
+        """Release the grabbed camera"""
         self._cam.release()
         self.hasCamera = False
         print("Released Camera")
         
     def takeImage(self, imgName: str):
+        """Use the grabbed camera to take an image
+
+        Args:
+            imgName (str): the filename to save the image under
+        """
         if not self.hasCamera:
             self.grabCamera()
         ret, frame = self._cam.read()
@@ -64,15 +91,31 @@ class PuzzleGenerator:
         self._grid = grid.tolist()
     
     def callMaze(self, imageFile: str, falsePaths: bool = True, lightMode: bool = False):
+        """Create a Maze puzzle.
+
+        Args:
+            imageFile (str): filename for the image of the path grid
+            falsePaths (bool, optional): whether to generate false paths. Defaults to True.
+            lightMode (bool, optional): whether to create the maze in light mode. Defaults to False.
+        """
+        # get the grid from the image
         self.captureGrid(imageFile)
         maze = Maze(self._grid)
+        # print the maze to console
         print(maze)
+        # create params for generate function
         coords = (self._x, self._y, self._z)
         cmdCoords = (self._x + maze.max_x + 5, self._y + 1, self._z + maze.max_y)
         cmdDirection = (0, -1)
         
+        # generate the list of commands
         commands = generate(maze, coords, cmdCoords, cmdDirection, {"falsePaths": falsePaths, "light": lightMode})
         
+        # !!!! DON'T UNCOMMENT !!!!
+        # ########################
+        # This was an attempt to streamline edits of an existing maze in Minecraft. It is incomplete and will break the maze
+        # if any edits were made to the maze as opposed to simple additions.
+        #
         # compare old commands with new ones to only execute the difference
         # the command block commands are compared this way, the commands which build the actual maze all have to
         # be run to allow for a different generation of false paths with the new user-defined path
@@ -83,10 +126,13 @@ class PuzzleGenerator:
         #         break
         #     if c not in self._commands:
         #         to_execute.append(c)
+        # ########################
         
+        # save commands to file
         with open("build.mcfunction", "w") as f:
             f.write("\n".join(commands))
-            
+        
+        # change window to Minecraft, then run the commands
         setFocus(".*Minecraft.*")
         subprocess.call(["runCommands\\runCommands.exe", "build.mcfunction"])
         
